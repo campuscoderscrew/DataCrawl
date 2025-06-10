50eaf43b2d52e93f8c108dd9414b9f4f2219cbfb
NickYuannn <nk.yuan2005@gmail.com>
2025-06-10 10:55:58 -0400

    changes to error text and spacing

--- src/Components/SearchMessageErrors.jsx
+++ src/Components/SearchMessageErrors.jsx
@@ -13,15 +13,17 @@ function SearchMessageErrors(props) {
             className="bg-black/60 text-red-600 w-70 h-80 py-10 rounded-lg shadow-2xl flex flex-col items-center gap-6 relative"
             onClick={(e) => e.stopPropagation()}
           >
-            <h1 className="font-bold">Error</h1>
-            <div>
+            <h1 className="font-bold text-center">
+              Unable to generate results for the following reasons
+            </h1>
+            <div className="flex flex-col items-start gap-3">
               {props.errors.map((err, i) => (
                 <div key={i}>{err}</div>
               ))}
             </div>
             <button
               onClick={props.onClose}
-              className="text-2xl font-bold absolute bottom-4 "
+              className="text-2xl font-bold absolute bottom-4 hover:text-red-800 "
             >
               <IoCloseCircleOutline />
             </button>
