d43db6d4555d760cf516fa9fe3f786ab9212f745
NickYuannn <nk.yuan2005@gmail.com>
2025-06-09 22:36:14 -0400

    created search errors

---
+++ src/Components/SearchMessageErrors.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
+function SearchMessageErrors(props) {
+  return (
+    <div>
+      {props.messages.length > 0 && (
+        <div
+          className="fixed inset-0 flex items-center justify-center z-50 font-[Helvetica]"
+          onClick={props.onClose}
+        >
+          <div
+            className="bg-black/60 text-red-600 w-60 h-70 py-10 rounded-lg shadow-2xl flex flex-col items-center gap-6 relative"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <h1 className="font-bold">Error</h1>
+            <div>
+              {props.errors.map((msg, i) => (
+                <div key={i}>{msg}</div>
+              ))}
+            </div>
+            <button
+              onClick={props.onClose}
+              className="text-2xl font-bold absolute bottom-4 "
+            >
+              <IoCloseCircleOutline />
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchMessageErrors;
