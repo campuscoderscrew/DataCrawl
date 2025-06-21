88b4ee0431ba53ee662f359cc07475c62a6e34bb
elianna-2004 <elianna@terpmail.umd.edu>
2025-06-20 23:28:55 -0400

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,41 +3,46 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="p-4 bg-gray-800 text-white w-full flex justify-between items-center absolute top-0">
+    <nav className="p-4 bg-transparent text-white w-full flex justify-between items-center absolute top-0 z-1000">
+      // Logo & Company Name
       <div className="ml-15">
+        <img src = "Logo-basic.svg" alt="" />
         <Link to="/" className="text-white hover:text-gray-400">
           <h1>DataCrawl</h1>
         </Link>
       </div>
 
+      // Menu Items with Background Button
       <div className="mr-15">
-        <ul className="grid grid-cols-3 gap-25">
-          <li>
-            <Link to="/Usage" className="text-white hover:text-gray-400">
-              Usage
-            </Link>
-          </li>
-          <li>
-            <Link to="/About" className="text-white hover:text-gray-400">
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="/Support" className="text-white hover:text-gray-400">
-              Support
-            </Link>
-          </li>
-          <li>
-            <Link to="/Investors" className="text-white hover:text-gray-400">
-              Investors
-            </Link>
-          </li>
-          <li>
-            <Link to="/Team" className="text-white hover:text-gray-400">
-              Team
-            </Link>
-          </li>
-        </ul>
+        <a href="#" class="p-3 px-6 pt-2 text-white bg-gray-700 rounded-full baseline">
+          <ul className="grid grid-cols-3 gap-25">
+            <li>
+              <Link to="/Usage" className="text-white hover:text-gray-400">
+                Usage
+              </Link>
+            </li>
+            <li>
+              <Link to="/About" className="text-white hover:text-gray-400">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/Support" className="text-white hover:text-gray-400">
+                Support
+              </Link>
+            </li>
+            <li>
+              <Link to="/Investors" className="text-white hover:text-gray-400">
+                Investors
+              </Link>
+            </li>
+            <li>
+              <Link to="/Team" className="text-white hover:text-gray-400">
+                Team
+              </Link>
+            </li>
+          </ul>
+        </a>
       </div>
     </nav>
   );
