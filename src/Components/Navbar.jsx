66440784c7ad1b31a0c64981a310578d38e1f78f
Henry Gardiner <henrygdnr@gmail.com>
2025-06-21 01:20:57 -0400

    Navbar Improvements

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="p-4 bg-transparent text-white w-full flex justify-between items-center absolute top-0 z-1000">
+    <nav className="py-6 px-4 bg-transparent text-white w-full flex justify-between items-center absolute top-0 z-1000">
       {/* Logo & Company Name */}
       <div className="ml-15 flex items-center space-x-3">
         <img src = "Logo-basic.svg" alt="" />
@@ -13,36 +13,34 @@ function Navbar() {
       </div>
 
       {/* Menu Items with Background Button */}
-      <div className="mr-15 bg-[#313131] rounded-full baseline">
-        <a href="#" class="p-3 px-6 pt-2 text-white">
-          <ul className="grid grid-cols-5 gap-20">
-            <li>
-              <span className="text-white hover:text-gray-400">
-                Usage
-              </span>
-            </li>
-            <li>
-              <span className="text-white hover:text-gray-400">
-                About
-              </span>
-            </li>
-            <li>
-              <span className="text-white hover:text-gray-400">
-                Support
-              </span>
-            </li>
-            <li>
-              <span className="text-white hover:text-gray-400">
-                Investors
-              </span>
-            </li>
-            <li>
-              <span className="text-white hover:text-gray-400">
-                Team
-              </span>
-            </li>
-          </ul>
-        </a>
+      <div className="mr-15 bg-[#313131] rounded-full px-6 py-3 text-white">
+        <ul className="font-semibold flex space-x-12">
+          <li>
+            <span className="text-white hover:text-gray-400">
+              Usage
+            </span>
+          </li>
+          <li>
+            <span className="text-white hover:text-gray-400">
+              About
+            </span>
+          </li>
+          <li>
+            <span className="text-white hover:text-gray-400">
+              Support
+            </span>
+          </li>
+          <li>
+            <span className="text-white hover:text-gray-400">
+              Investors
+            </span>
+          </li>
+          <li>
+            <span className="text-white hover:text-gray-400">
+              Team
+            </span>
+          </li>
+        </ul>
       </div>
     </nav>
   );
