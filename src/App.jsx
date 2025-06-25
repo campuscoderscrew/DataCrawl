4df9cd6804a6de6f0386123f75ef1c247cde07a5
NickYuannn <nk.yuan2005@gmail.com>
2025-06-25 12:02:47 -0400

    changed routing for search

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import Search from "./Pages/Search";
 import Results from "./Pages/Results";
 import Navbar from "./Components/Navbar";
 
-
 function App() {
   const [data, setData] = React.useState([]);
 
   const handleData = (formData) => {
     console.log("Data received in App component:", formData);
     setData(formData);
-  }
-  
+  };
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Search onSearchSubmit={handleData} />} />
+        <Route
+          path="/search"
+          exact
+          element={<Search onSearchSubmit={handleData} />}
+        />
         <Route path="/results" exact element={<Results resultsData={data} />} />
       </Routes>
     </Router>
