cfa2b6fe8d5b4f5c7ed59a9adf35426c4330e017
MeronAGithub <115681778+MeronAGithub@users.noreply.github.com>
2025-07-12 02:08:01 -0400

    animations added to landing + regsitration, links working

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,35 +4,47 @@ import React from "react"
 
 import { motion } from 'motion/react';
 
-export default function Register() {
+export default function Register(props) {
 
-    let [newUser, setNewUser] = React.useState(false);
+    let [newUser, setNewUser] = React.useState(props.newUse);
+
+    const [hasMounted, setHasMounted] = React.useState(false);
+
+
+    React.useEffect(() => {
+        setNewUser(props.newUse);
+    }, [props.newUse]);
+
+    React.useEffect(() => {
+        // Triggered after first render
+        setHasMounted(true);
+    }, []);
+
+    const rotation = newUser ? 180 : 0;
 
     return (
-        <>
+        <div className="absolute overflow-hidden">
             <div className="bg w-screen h-screen bg-[#252525] box-border overflow-hidden">
                 <div className="absolute w-full h-full top-0 left-0 bg-[linear-gradient(135deg,_#1e1e1e00,_#131313)] z-1"></div>
                 <img src="/landing-background.svg" className="absolute top-0 left-0 object-cover bg-[#252525] w-full h-full" />
             </div>
-            {/* {newUser ?
-                <motion.div className="signup-component">
-                    <SignUp setNewUser={setNewUser} />
-                </motion.div>
-                :
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5 }}
-                    viewport={{ once: true }}
-                    className="login-component">
-                    <Login setNewUser={setNewUser} />
-                </motion.div>
-            } */}
 
-            <div className="perspective-[1000] h-135 w-100 z-2 absolute top-[50%] transform -translate-y-[50%] right-[50%] translate-x-[50%] md:translate-x-[0] md:right-[10%] lg:right-[15%]">
+            <motion.div
+                initial={{ x: '5vw', opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{
+                    delay: 0,        // 1 second delay
+                    duration: 0.75,   // animation duration
+                    ease: 'easeOut'  // smooth easing
+                }}
+
+
+                className="perspective-[1000] h-135 w-100 z-2 absolute top-[50%] transform -translate-y-[50%] right-[50%] translate-x-[50%] md:translate-x-[0] md:right-[10%] lg:right-[15%]">
                 <motion.div
-                    className="w-full h-full preserve-3d relative transition-transform duration-700"
-                    animate={{ rotateY: newUser ? 180 : 0 }}
+                    className="w-full h-full preserve-3d relative"
+                    initial={{ rotateY: rotation }}
+                    animate={hasMounted ? { rotateY: rotation } : false}
+                    transition={{ duration: 0.7 }}
                     style={{
                         transformStyle: "preserve-3d",
                     }}
@@ -46,7 +58,7 @@ export default function Register() {
                         <SignUp setNewUser={setNewUser} />
                     </div>
                 </motion.div>
-            </div>
-        </>
+            </motion.div >
+        </div>
     )
 }
